1d04ce46813b12064c2b63e3eaa00b33f679a447
Angel Contreras Dueñas <c3po.angel@gmail.com>
2025-04-08 11:30:42 -0600

    api calls

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,7 +4,7 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import type { SearchType } from "../../types";
 import Alert from "../Alert/Alert";
 type FormProps = {
-  fetchWeather: () => void;
+  fetchWeather: (search: SearchType) => Promise<void>;
 };
 export default function Form({ fetchWeather }: FormProps) {
   const [search, setSearch] = useState<SearchType>({
@@ -26,7 +26,7 @@ export default function Form({ fetchWeather }: FormProps) {
       setAlert("Todos los campos son obligatorios");
       return;
     }
-    fetchWeather();
+    fetchWeather(search);
   };
 
   return (
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,6 +1,45 @@
+import axios from "axios";
+import type { SearchType, Weather } from "../types";
+function isWeatherResponse(weather: unknown): weather is Weather {
+  return (
+    Boolean(weather) &&
+    typeof weather === "object" &&
+    typeof (weather as Weather).name === "string" &&
+    typeof (weather as Weather).main.temp === "number" &&
+    typeof (weather as Weather).main.temp_max === "number" &&
+    typeof (weather as Weather).main.temp_min === "number"
+  );
+}
 export default function useWeather() {
-  const fetchWeather = () => {
-    console.log("...consultando");
+  const appId = import.meta.env.VITE_API_KEY;
+  const getGeo = async (
+    search: SearchType
+  ): Promise<{ lat: number; lon: number }> => {
+    const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+    const { data } = await axios.get(geoURL);
+    const lat = data[0].lat;
+    const lon = data[0].lon;
+    return { lat, lon };
+  };
+  const getWeather = async (lat: number, lon: number): Promise<Weather> => {
+    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+    //type guards
+    const { data } = await axios.get(weatherURL);
+    const result = isWeatherResponse(data);
+    if (result) {
+      return data;
+    } else {
+      throw new Error("Objeto de clima incorrecto");
+    }
+  };
+  const fetchWeather = async (search: SearchType) => {
+    try {
+      const { lat, lon } = await getGeo(search);
+      const weather = await getWeather(lat, lon);
+      console.log(weather);
+    } catch (error) {
+      console.error(error);
+    }
   };
   return { fetchWeather };
 }
